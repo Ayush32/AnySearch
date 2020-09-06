e49ba7fd2f2c785fd33139651c3677e7cc65acf0
Ayush Gupta <721ayush@gmail.com>
2020-09-06 20:17:44 +0530

    imagecard component

---
+++ src/components/ImageCard.js
@@ -0,0 +1,20 @@
+/*
+ *   Copyright (c) 2020
+ *   All rights reserved.
+ */
+import React from "react";
+
+class ImageCard extends React.Component {
+  render() {
+    return (
+      <div>
+        <img
+          alt={this.props.image.description}
+          src={this.props.image.urls.regular}
+        />
+      </div>
+    );
+  }
+}
+
+export default ImageCard;
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -4,11 +4,12 @@
  */
 import "./ImageList.css";
 import React from "react";
+import ImageCard from "./ImageCard";
 
 const ImageList = (props) => {
   const images = props.images.map(({ description, id, urls }) => {
     // eslint-disable-next-line
-    return <img alt={description} key={id} src={urls.regular} />;
+    return <ImageCard key={id} image={image} />;
   });
   return <div className="image-list">{images}</div>;
 };
