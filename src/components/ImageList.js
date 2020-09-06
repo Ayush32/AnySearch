523fcefb0f87a43e89756654fc8ed9f0cfaf331c
Ayush Gupta <721ayush@gmail.com>
2020-09-06 19:53:03 +0530

    grid css

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -5,9 +5,9 @@
 import React from "react";
 
 const ImageList = (props) => {
-  const images = props.images.map((image) => {
+  const images = props.images.map(({ description, id, urls }) => {
     // eslint-disable-next-line
-    return <img src={image.urls.regular} />;
+    return <img alt={description} key={id} src={urls.regular} />;
   });
   return <div>{images}</div>;
 };
