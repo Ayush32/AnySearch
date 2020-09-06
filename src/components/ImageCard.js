e2bb507e149707c3eff896bd12b9fc96572d898d
Ayush Gupta <721ayush@gmail.com>
2020-09-06 20:26:46 +0530

    react ref

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -5,11 +5,19 @@
 import React from "react";
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    console.log(this.imageRef);
+  }
   render() {
     const { description, urls } = this.props.image;
     return (
       <div>
-        <img alt={description} src={urls.regular} />
+        <img ref={this.imageRef} alt={description} src={urls.regular} />
       </div>
     );
   }
